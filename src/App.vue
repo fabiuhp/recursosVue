4961140e2a9fcbb3f959e63dc0c3f174c7e2b5ed
Fabio Pereira <fabiuhp@msn.com>
2021-03-03 21:42:46 -0300

    começo do projeto

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      message: 'Hello'
+    }
+  },
+  beforeCreate() {
+    console.log('Antes da criação')
+  },
+  created() {
+    console.log('Depois da criação')
+  }
+})
+</script>
